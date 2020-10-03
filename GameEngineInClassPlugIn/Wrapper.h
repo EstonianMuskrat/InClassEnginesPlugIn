bb45cf941893be3ea8547e1ac4c9f8c1d5604bf0
EstonianMuskrat <44206553+EstonianMuskrat@users.noreply.github.com>
2020-10-03 12:33:35 -0400

    Add files via upload

---
+++ GameEngineInClassPlugIn/Wrapper.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __WRAPPER__
+#define __WRAPPER__
+
+#include "PluginSettings.h"
+#include "Vector3D.h"
+
+#ifndef __cplusplus
+extern "C"
+{
+#endif
+
+	PLUGIN_API int GetID();
+
+	PLUGIN_API void SetID(int id);
+
+	PLUGIN_API Vector3D GetPosition();
+
+	PLUGIN_API void SetPosition(float x, float y, float z);
+
+#ifndef __cplusplus
+}
+#endif
+
+#endif /* defined (__WRAPPER__) */
